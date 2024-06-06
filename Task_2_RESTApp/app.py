9942b8620ee85bcbb7bdaeb02e9e03a26892a160
Nikhil <x20179529@students.ncirl.ie>
2024-06-06 14:04:47 +0100

    Add: created and connected sportsbook db with schema structure from schema.sql

--- Task_2_RESTApp/app.py
+++ Task_2_RESTApp/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import sqlite3
+from datetime import datetime
+
 
 app = Flask(__name__)
+DATABASE = 'sportsbook.db'
+
+def get_db():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route("/")
 def index():
