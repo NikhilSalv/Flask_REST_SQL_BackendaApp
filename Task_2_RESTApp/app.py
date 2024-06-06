9d1b32c17d3f49fabc48ea5ebbd7b711bb6057b3
Nikhil <x20179529@students.ncirl.ie>
2024-06-06 16:25:23 +0100

    Add: Search route for Sports on name & active fields

--- Task_2_RESTApp/app.py
+++ Task_2_RESTApp/app.py
@@ -21,6 +21,28 @@ def create_sport():
     conn.commit()
     return jsonify({"id": cur.lastrowid}), 201
 
+@app.route('/sports', methods=['GET'])
+def get_sports():
+    query = "SELECT * FROM sports"
+    filters = []
+    params = []
+
+    if 'name' in request.args:
+        filters.append("name LIKE ?")
+        params.append(f"%{request.args['name']}%")
+    
+    if 'active' in request.args:
+        filters.append("active = ?")
+        params.append(request.args['active'])
+    
+    if filters:
+        query += " WHERE " + " AND ".join(filters)
+    
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute(query, params)
+    sports = cur.fetchall()
+    return jsonify([dict(row) for row in sports])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
